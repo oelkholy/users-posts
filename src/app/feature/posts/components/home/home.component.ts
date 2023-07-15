690cee72c5101b2674b821a1935c7a63e214c53b
omar <omar.khoil@42.solutions>
2023-07-15 17:59:43 +0300

    Adding filter operator to selected User observable

--- src/app/feature/posts/components/home/home.component.ts
+++ src/app/feature/posts/components/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable, switchMap } from 'rxjs';
+import { Observable, filter, switchMap } from 'rxjs';
 import { ApiService } from 'src/app/core/services/api.service';
 import { UserService } from 'src/app/core/services/user.service';
 import { Post } from '../../models/post.model';
@@ -26,7 +26,8 @@ export class HomeComponent implements OnInit {
   getUserPosts() {
     this.userPosts$ = this.userService.selectedUser$
       .pipe(
-        switchMap(user => this.apiService.get<Post[]>(`posts?userId=${user?.id}`))
+        filter(user => !!user),
+        switchMap(user => this.apiService.get<Post[]>(`posts?userId=${user?.id}`)),
       )
   }
 
