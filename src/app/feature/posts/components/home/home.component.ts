e0cfe83dcc7a27c07fc3176dca8c6d575b7d7ffd
omar <omar.khoil@42.solutions>
2023-07-15 18:28:13 +0300

    Loading posts on user changes with opacity .3

--- src/app/feature/posts/components/home/home.component.ts
+++ src/app/feature/posts/components/home/home.component.ts
@@ -1,18 +1,21 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable, filter, switchMap } from 'rxjs';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { BehaviorSubject, Observable, delay, distinctUntilChanged, filter, finalize, switchMap } from 'rxjs';
 import { ApiService } from 'src/app/core/services/api.service';
 import { UserService } from 'src/app/core/services/user.service';
 import { Post } from '../../models/post.model';
+import { User } from 'src/app/core/models/user.model';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HomeComponent implements OnInit {
 
   userPosts$: Observable<Post[]>;
-  postCharactersLength = 150
+  postCharactersLength = 150;
+  isLoading = new BehaviorSubject<boolean>(false)
 
   constructor(
     public userService: UserService,
@@ -20,14 +23,25 @@ export class HomeComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.getUserPosts();
+    this.checkSelectedUser();
   }
 
-  getUserPosts() {
+  checkSelectedUser() {
     this.userPosts$ = this.userService.selectedUser$
       .pipe(
         filter(user => !!user),
-        switchMap(user => this.apiService.get<Post[]>(`posts?userId=${user?.id}`)),
+        distinctUntilChanged(), // Prevent duplicate Api requests while clicking on the same user (Caching also can fix)
+        switchMap(user => this.getUserPosts(user!)),
+      )
+  }
+
+  getUserPosts(user: User) {
+    this.isLoading.next(true);
+    return this.apiService
+      .get<Post[]>(`posts?userId=${user?.id}`)
+      .pipe(
+        delay(150),
+        finalize(()=> this.isLoading.next(false))
       )
   }
 
