c9f28d3619c7b3d7008d8930d4309a00ee5f21fb
omar <omar.khoil@42.solutions>
2023-07-16 14:21:22 +0300

    Adding Testing to LimitCharactersPipe

--- src/app/shared/pipes/limit-characters.pipe.spec.ts
+++ src/app/shared/pipes/limit-characters.pipe.spec.ts
@@ -1,8 +1,34 @@
+import { TestBed } from '@angular/core/testing';
 import { LimitCharactersPipe } from './limit-characters.pipe';
 
 describe('LimitCharactersPipe', () => {
+
+  let pipe: LimitCharactersPipe;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [LimitCharactersPipe]
+    });
+    pipe = TestBed.inject(LimitCharactersPipe);
+  });
+
   it('create an instance', () => {
     const pipe = new LimitCharactersPipe();
     expect(pipe).toBeTruthy();
   });
+
+  it('should limit the number of characters in a string', () => {
+    const value = 'Hello, world!';
+    const limit = 5;
+    const result = pipe.transform(value, limit);
+    expect(result).toEqual('Hello...');
+  });
+
+  it('should return the original string if it is shorter than the limit', () => {
+    const value = 'Hello, world!';
+    const limit = 20;
+    const result = pipe.transform(value, limit);
+    expect(result).toEqual('Hello, world!');
+  });
+
 });
