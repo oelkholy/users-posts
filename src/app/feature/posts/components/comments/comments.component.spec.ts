8f363ac8280dfb3a245dc4f8e552877e0484d002
omar <omar.khoil@42.solutions>
2023-07-16 14:54:09 +0300

    Adding Testing to CommentsComponent

--- src/app/feature/posts/components/comments/comments.component.spec.ts
+++ src/app/feature/posts/components/comments/comments.component.spec.ts
@@ -2,16 +2,20 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CommentsComponent } from './comments.component';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 describe('CommentsComponent', () => {
   let component: CommentsComponent;
   let fixture: ComponentFixture<CommentsComponent>;
+  let modalService: jasmine.SpyObj<NgbModal>;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
+  beforeEach(async () => {
+    modalService = jasmine.createSpyObj('NgbModal', ['dismissAll']);
+    await TestBed.configureTestingModule({
       declarations: [CommentsComponent],
-      imports: [HttpClientTestingModule]
-    });
+      imports: [HttpClientTestingModule],
+      providers: [{ provide: NgbModal, useValue: modalService }]
+    }).compileComponents();
     fixture = TestBed.createComponent(CommentsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -20,4 +24,10 @@ describe('CommentsComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should dismiss the modal', () => {
+    component.closeModal();
+    expect(modalService.dismissAll).toHaveBeenCalled();
+  });
+
 });
